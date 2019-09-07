9fe00cb8482eff36d3d19a56c7c3e9e21462df30
Avinash Rath <avinash.rrath@gmail.com>
2019-09-07 16:09:44 +0530

    Session 2 - 7/9/19

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,61 @@ class HomePage extends StatelessWidget {
               backgroundColor: whatsAppColor,
               title: Text('WhatsApp'),
             ),
-            body: Container(),
+            body: ListView.builder(
+              itemCount: 10,
+                itemBuilder:
+                (BuildContext context, int count) {
+                return WhatsAppCard();
+                }),
           );
+  }
+}
+
+class WhatsAppCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 95.0,
+      padding: EdgeInsets.all(10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 10.0),
+            child: Icon(Icons.account_circle, size: 75.0,
+              color: Colors.grey.withOpacity(0.7),),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Avinash', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0
+                ),),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(Icons.check, size: 15.0, color: Colors.grey,),
+                    Text('Hi!', style: TextStyle(
+                        color: Colors.grey
+                    ),)
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 14.0),
+            child: Text('15:32'),
+          ),
+        ],
+      ),
+    );
   }
 }
