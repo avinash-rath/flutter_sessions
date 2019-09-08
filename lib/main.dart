2a6ab9116a0cae4f30546687355cbe9f92a65819
Avinash Rath <avinash.rrath@gmail.com>
2019-09-08 08:38:35 +0530

    dartfmt Session-2 7/9/2019

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-          home: HomePage()
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
@@ -18,17 +14,16 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     Color whatsAppColor = Color(0xFF32642E);
     return Scaffold(
-            appBar: AppBar(
-              backgroundColor: whatsAppColor,
-              title: Text('WhatsApp'),
-            ),
-            body: ListView.builder(
-              itemCount: 10,
-                itemBuilder:
-                (BuildContext context, int count) {
-                return WhatsAppCard();
-                }),
-          );
+      appBar: AppBar(
+        backgroundColor: whatsAppColor,
+        title: Text('WhatsApp'),
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int count) {
+            return WhatsAppCard();
+          }),
+    );
   }
 }
 
@@ -43,8 +38,11 @@ class WhatsAppCard extends StatelessWidget {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.only(right: 10.0),
-            child: Icon(Icons.account_circle, size: 75.0,
-              color: Colors.grey.withOpacity(0.7),),
+            child: Icon(
+              Icons.account_circle,
+              size: 75.0,
+              color: Colors.grey.withOpacity(0.7),
+            ),
           ),
           Padding(
             padding: EdgeInsets.only(top: 15.0),
@@ -52,17 +50,22 @@ class WhatsAppCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                Text('Avinash', style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0
-                ),),
+                Text(
+                  'Avinash',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
                 Row(
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
-                    Icon(Icons.check, size: 15.0, color: Colors.grey,),
-                    Text('Hi!', style: TextStyle(
-                        color: Colors.grey
-                    ),)
+                    Icon(
+                      Icons.check,
+                      size: 15.0,
+                      color: Colors.grey,
+                    ),
+                    Text(
+                      'Hi!',
+                      style: TextStyle(color: Colors.grey),
+                    )
                   ],
                 ),
               ],
