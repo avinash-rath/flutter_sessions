8ae076bbe9e72a639d7b46b40d4efc068427dc54
Avinash Rath <avinash.rrath@gmail.com>
2019-09-23 17:13:12 +0530

    intro to Stateful Widgets - 23/09/2019

--- lib/main.dart
+++ lib/main.dart
@@ -5,81 +5,53 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RespondEvents(),
+    );
   }
 }
 
-class HomePage extends StatelessWidget {
+class RespondEvents extends StatefulWidget {
+  @override
+  _RespondEventsState createState() => _RespondEventsState();
+}
+
+class _RespondEventsState extends State<RespondEvents> {
+  String text = 'Type into  the `TextFormField`';
+  TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    Color whatsAppColor = Color(0xFF32642E);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: whatsAppColor,
-        title: Text('WhatsApp'),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              text,
+              style: TextStyle(fontSize: 20.0),
+            ),
+            TextFormField(
+              controller: _controller,
+              onChanged: (String something) {
+                setState(() {
+                  text=_controller.text;
+                });
+              },
+            ),
+            // RaisedButton(
+            //   child: Text('Submit'),
+            //   onPressed: () {
+            //     setState(() {
+            //       text = _controller.text;
+            //     });
+            //   },
+            // )
+          ],
+        ),
       ),
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int count) {
-            return WhatsAppCard();
-          }),
     );
   }
 }
 
-class WhatsAppCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 95.0,
-      padding: EdgeInsets.all(10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 10.0),
-            child: Icon(
-              Icons.account_circle,
-              size: 75.0,
-              color: Colors.grey.withOpacity(0.7),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(
-                  'Avinash',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Icon(
-                      Icons.check,
-                      size: 15.0,
-                      color: Colors.grey,
-                    ),
-                    Text(
-                      'Hi!',
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 14.0),
-            child: Text('15:32'),
-          ),
-        ],
-      ),
-    );
-  }
-}
+
