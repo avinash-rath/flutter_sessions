f7a3b876566d6b2888ee19c044d26682ff3b13b2
Avinash Rath <avinash.rrath@gmail.com>
2019-10-12 16:22:48 +0530

    session -3 Part 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,66 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>  with SingleTickerProviderStateMixin {
+  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+  TabController controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = new TabController(
+    length: 3,
+    initialIndex: 0,
+    vsync: this,
+  );
+  }
   @override
   Widget build(BuildContext context) {
     Color whatsAppColor = Color(0xFF32642E);
     return Scaffold(
       appBar: AppBar(
+        bottom: TabBar(
+          controller: controller,
+        ),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search),
+          color: Colors.white,
+          onPressed: (){
+            //TODO
+          },),
+          IconButton(icon: Icon(Icons.more_vert),
+          onPressed: (){
+            //TODO
+          },)
+        ],
         backgroundColor: whatsAppColor,
         title: Text('WhatsApp'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          //TODO
+          
+        },
+        child: Icon(Icons.chat),
+        backgroundColor: whatsAppColor,
       ),
       body: ListView.builder(
           itemCount: 10,
