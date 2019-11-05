d502a7ed469a5fcdb303dcd53ef36c08167dc195
sriramRavanam <sriramravanam1@gmail.com>
2019-11-05 21:03:48 +0530

    idk

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\App_Dev\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\App_Dev\Dev\documented version\my\flutter_sessions"
+export "FLUTTER_APPLICATION_PATH=D:\App_Dev\Dev\documented version\flutter_sessions"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
